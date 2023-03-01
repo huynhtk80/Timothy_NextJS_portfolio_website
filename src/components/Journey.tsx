672d4294a927b34fbd727c31644fb466b19243af
Timothy H <huynh.tk80@gmail.com>
2023-02-28 17:02:43 -0700

    fix: key added to map

--- src/components/Journey.tsx
+++ src/components/Journey.tsx
@@ -85,6 +85,7 @@ function Journey() {
             <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-700">
               {experience.map((exp) => (
                 <div
+                  key={exp.title}
                   className={
                     "flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-6 sm:before:h-6 sm:before:rounded-full sm:before:left-[-39px] sm:before:z-[1] before:bg-blue-400 sm:before:text-center " +
                     (exp.type === "school"
